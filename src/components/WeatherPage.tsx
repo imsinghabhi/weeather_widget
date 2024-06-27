64e1819b30324af1a89f4bbedd49aa79bde6800e
Abhishek Singh <singh.abhishek@binmile.com>
2024-06-27 11:36:27 +0530

    Update code to reflect changes made

--- src/components/WeatherPage.tsx
+++ src/components/WeatherPage.tsx
@@ -14,19 +14,11 @@ const WeatherPage: React.FC = () => {
 
   const fetchWeatherData = useCallback(async (city: City) => {
     try {
-      // Manually constructing the URL with query parameters
+     
       const url = `https://api.open-meteo.com/v1/forecast?latitude=${city.lat}&longitude=${city.lng}&daily=weather_code,temperature_2m_max,temperature_2m_min&forecast_days=1`;
-  
-      // Making the GET request
       const response = await axios.get(url);
-  
-      // Extracting data
       const weather = response.data.daily;
-  
-      // Finding minimum and maximum temperatures from the arrays
-      
-  
-      // Setting the state with the fetched weather data
+
       setWeatherData({
         minTemp: weather.temperature_2m_min[0],
         maxTemp: weather.temperature_2m_max[0],
@@ -40,16 +32,16 @@ const WeatherPage: React.FC = () => {
   
 
  
-//   useEffect(() => {
-//     const savedCity = localStorage.getItem('selectedCity');
-//     if (savedCity) {
-//       const city = JSON.parse(savedCity) as City;
-//       setSelectedCity(city);
-//       fetchWeatherData(city);
-//     }
-//   }, [fetchWeatherData]);
+  useEffect(() => {
+    const savedCity = localStorage.getItem('selectedCity');
+    if (savedCity) {
+      const city = JSON.parse(savedCity) as City;
+      setSelectedCity(city);
+      fetchWeatherData(city);
+    }
+  }, [fetchWeatherData]);
 
-  // Fetch weather data and save the selected city to local storage whenever it changes
+
   useEffect(() => {
     if (selectedCity) {
       fetchWeatherData(selectedCity);
@@ -57,15 +49,15 @@ const WeatherPage: React.FC = () => {
     }
   }, [selectedCity, fetchWeatherData]);
 
-  // Set up an interval to refresh the weather data every 10 minutes
+  
   useEffect(() => {
     const interval = setInterval(() => {
       if (selectedCity) {
         fetchWeatherData(selectedCity);
       }
-    }, 600000); // 10 minutes in milliseconds
+    }, 600000); 
 
-    return () => clearInterval(interval); // Clear the interval on component unmount
+    return () => clearInterval(interval); 
   }, [selectedCity, fetchWeatherData]);
 
   return (
